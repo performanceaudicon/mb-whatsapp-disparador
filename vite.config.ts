39d5e69958f43656a98f31c9892610381c08ab34
performanceaudicon <performance@audiconsultoriadigital.com.br>
2025-07-07 18:56:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,3 @@ export default defineConfig({
   base: '/mb-whatsapp-disparador/',   // ← igual ao nome do repo
   plugins: [react()],
 })
-
-// src/config.ts
-export const WEBHOOK = 'https://primary-production-7688.up.railway.app/webhook/send-group-messages';
